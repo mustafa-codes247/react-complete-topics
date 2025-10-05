c6154acf3e14ddb88711b10659d5518c197a1de7
Mustafa Altaf <usermustafa500@gmail.com>
2025-10-06 00:43:01 +0500

    class wise folders made

---
+++ all-topics/src/class1-basics-24-sep-25/Functionsyntax.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// funciton is made with 
+const Functionsyntax = () => {
+  return (
+    <div>
+        {/* this is basic syntax of function */}
+        {/* xml is written inside these fucntions , its similar to html but its not */}
+      
+    </div>
+  )
+}
+
+// this is export default where only one function per conponent can be made/called 
+// if we want to make/call multiple function from same components , we use custom export/import
+export default Functionsyntax
