84237a614d8a18f6dc566962242174c9e7b23f0c
Mustafa Altaf <usermustafa500@gmail.com>
2025-11-03 00:05:53 +0500

    theme tweeking

--- all-classwork/src/c7-routing-component-8-oct-25/Home.jsx
+++ all-classwork/src/c7-routing-component-8-oct-25/Home.jsx
@@ -10,20 +10,19 @@ const Home = () => {
     <div className='flex justify-center'>
       <div className='text-start'>
     <ol>
-      <p className='text-3xl py-2 font-bold text-center bg-blue-500 rounded-md'>Till now I have learnt :</p>
-      <li className='bg-blue-500 py-2 my-2 rounded-md '>1-Routing of pages without page reload</li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>2-components making and using them</li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>3-Material UI </li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>4-Tailwind CSS UI</li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>5-Props (properties) values that can be passed from parent to child <Link to="props" className='hover:text-blue-500'>click</Link></li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>6-UseState for managing state (changing values of variables) </li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>7-UseEffect for performing side effect in website eg json data calling, setTimeOut etc </li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>8-useRef for targetting dom elements directly without component reRendering</li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>8-useContext for managing state globally to avoid props drilling</li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>9-redux toolkit to manage state globally wihtout using useState</li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>10-data fetching using axios</li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>11- useNavigate , nested routing</li>
-      <li className='bg-blue-500 py-2 my-2 rounded-md'>12- making custom hooks for reusing them as function</li>
+      <p className='text-3xl py-2 font-bold text-center bg-blue-200 rounded-md'>Till now I have learnt :</p>
+      <li className='bg-blue-100 py-2 my-2 rounded-md '>1-Routing of pages without page reload</li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>2-components making and using them</li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>3-Material UI </li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>4-Tailwind CSS UI</li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>5-Props (properties) values that can be passed from parent to child <Link to="props" className='hover:text-blue-100'>click</Link></li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>6-UseState for managing state (changing values of variables) </li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>7-UseEffect for performing side effect in website eg json data calling, setTimeOut etc </li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>8-useRef for targetting dom elements directly without component reRendering</li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>8-useContext for managing state globally to avoid props drilling</li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>9-redux toolkit to manage state globally wihtout using useState</li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>10-data fetching using axios</li>
+      <li className='bg-blue-100 py-2 my-2 rounded-md'>11- useNavigate , nested routing</li>
     </ol>
     </div>
     </div>
